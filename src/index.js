e04b0bcc6d60f2b7fe615cb3c582dedfb49eecf5
Carter Hansen <carter@MacBook-Pro.local>
2025-03-31 00:16:50 -0400

    Add FirebaseProvider to index.js to enable concurrent editing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { FirebaseProvider } from './contexts/FirebaseContext';
 import './styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <FirebaseProvider>
+      <App />
+    </FirebaseProvider>
   </React.StrictMode>
 );
